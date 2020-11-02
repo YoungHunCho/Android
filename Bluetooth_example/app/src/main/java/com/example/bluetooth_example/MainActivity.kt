7cedadd9d7c0cbe7e63817bbea5a176eaaeacac7
cho_yh <cyh905@gmail.com>
2020-11-02 16:11:23 +0900

    connect bt

--- Bluetooth_example/app/src/main/java/com/example/bluetooth_example/MainActivity.kt
+++ Bluetooth_example/app/src/main/java/com/example/bluetooth_example/MainActivity.kt
@@ -8,17 +8,25 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
-import android.widget.Toast
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import java.io.InputStream
 import java.io.OutputStream
+import java.util.*
+import kotlin.text.Charsets.UTF_8
 
+
+// http://jinyongjeong.github.io/2018/09/27/bluetoothpairing/
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_ENABLE_BT = 3
 
-    var mDevices: Set<BluetoothDevice>? = null
+    var hc_06: BluetoothDevice? = null
+
     var mPairedDeviceCount = 0
     var mRemoteDevice: BluetoothDevice? = null
     var mSocket: BluetoothSocket? = null
@@ -26,8 +34,8 @@ class MainActivity : AppCompatActivity() {
     var mOutputStream: OutputStream? = null
     var mWorkerThread: Thread? = null
     var readBufferPositon = 0
-//    var readBuffer: ByteArray =
     var mDelimiter: Byte = 10
+    var mBluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,9 +60,13 @@ class MainActivity : AppCompatActivity() {
         stateFilter.addAction(BluetoothDevice.ACTION_PAIRING_REQUEST)
         registerReceiver(mBluetoothStateReceiver, stateFilter)
 
-        var mBluetoothAdapter: BluetoothAdapter? = null
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()   //블루투스 adapter 획득
-        mBluetoothAdapter.startDiscovery() //블루투스 기기 검색 시작
+
+//        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()   //블루투스 adapter 획득
+        findViewById<Button>(R.id.btn_bt_scan).setOnClickListener{
+            mBluetoothAdapter!!.startDiscovery() //블루투스 기기 검색 시작
+        }
+
+
     }
 
     override fun onDestroy() {
@@ -65,8 +77,7 @@ class MainActivity : AppCompatActivity() {
     var mBluetoothStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent) {
             val action = intent.action //입력된 action
-            Toast.makeText(context, "받은 액션 : $action", Toast.LENGTH_SHORT).show()
-            Log.d("Bluetooth action", action!!)
+            Log.d("#main_Bluetooth action", action!!)
             val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
             var name: String? = null
             if (device != null) {
@@ -98,23 +109,102 @@ class MainActivity : AppCompatActivity() {
                 BluetoothDevice.ACTION_FOUND -> {
                     val device_name = device!!.name
                     val device_Address = device.address
-                    Log.d("#main_device", device_name + " " + device_Address)
-                    //본 함수는 블루투스 기기 이름의 앞글자가 "GSM"으로 시작하는 기기만을 검색하는 코드이다
-//                    if (device_name != null && device_name.length > 4) {
-//                        Log.d("Bluetooth Name: ", device_name)
-//                        Log.d("Bluetooth Mac Address: ", device_Address)
-//                        if (device_name.substring(0, 3) == "GSM") {
-//                            bluetooth_device.add(device)
-//                        }
-//                    }
+                    Log.d("#main_device", "$device_name $device_Address")
+                    if (device_name == "HC-06") {
+                        hc_06 = device
+                        device.createBond()
+                        val uuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb")
+                        try {
+                            // 소켓 생성
+                            mSocket = hc_06!!.createRfcommSocketToServiceRecord(uuid)
+                            // RFCOMM 채널을 통한 연결, socket에 connect하는데 시간이 걸린다. 따라서 ui에 영향을 주지 않기 위해서는
+                            // Thread로 연결 과정을 수행해야 한다.
+                            mSocket!!.connect()
+                            mHandler.sendEmptyMessage(1)
+                        } catch (e: java.lang.Exception) {
+                            // 블루투스 연결 중 오류 발생
+                            mHandler.sendEmptyMessage(-1)
+                        }
+                    }
                 }
                 BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
-                    Log.d("Bluetooth", "Call Discovery finished")
-//                    StartBluetoothDeviceConnection() //원하는 기기에 연결
+                    Log.d("#main_Bluetooth", "Call Discovery finished")
                 }
                 BluetoothDevice.ACTION_PAIRING_REQUEST -> {
                 }
             }
         }
+    }
+
+    val mHandler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            if (msg.what === 1) // 연결 완료
+            {
+                try {
+                    //연결이 완료되면 소켓에서 outstream과 inputstream을 얻는다. 블루투스를 통해
+                    //데이터를 주고 받는 통로가 된다.
+                    mOutputStream = mSocket!!.outputStream
+                    mInputStream = mSocket!!.inputStream
+                    // 데이터 수신 준비
+                    beginListenForData()
+                } catch (e: java.lang.Exception) {
+                    e.printStackTrace()
+                }
+            } else {    //연결 실패
+                Log.e("#main", "no")
+                try {
+                    mSocket!!.close()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    //블루투스 데이터 수신 Listener
+    protected fun beginListenForData() {
+        val handler = Handler(Looper.getMainLooper())
+        val readBuffer = ByteArray(1024) //  수신 버퍼
+        readBufferPositon = 0 //   버퍼 내 수신 문자 저장 위치
+
+        // 문자열 수신 쓰레드
+        mWorkerThread = Thread {
+            while (!Thread.currentThread().isInterrupted) {
+                try {
+                    val bytesAvailable = mInputStream!!.available()
+                    if (bytesAvailable > 0) { //데이터가 수신된 경우
+                        val packetBytes = ByteArray(bytesAvailable)
+                        mInputStream!!.read(packetBytes)
+                        for (i in 0 until bytesAvailable) {
+                            val b = packetBytes[i]
+                            if (b == mDelimiter) {
+                                val encodedBytes = ByteArray(readBufferPositon)
+                                System.arraycopy(
+                                    readBuffer,
+                                    0,
+                                    encodedBytes,
+                                    0,
+                                    encodedBytes.size
+                                )
+                                val data = String(encodedBytes, UTF_8)
+                                readBufferPositon = 0
+                                handler.post(Runnable {
+                                    //수신된 데이터는 data 변수에 string으로 저장!! 이 데이터를 이용하면 된다.
+                                    val c_arr = data.toCharArray() // char 배열로 변환
+                                    Log.d("#main", data)
+                                })
+                            } else {
+                                readBuffer[readBufferPositon] = b
+                                readBufferPositon += 1
+                            }
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        //데이터 수신 thread 시작
+        mWorkerThread!!.start()
     }
 }
