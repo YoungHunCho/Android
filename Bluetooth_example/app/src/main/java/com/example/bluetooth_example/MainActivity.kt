abfdeb270723ea70d157ec0890af1d8207e9813f
cho_yh <cyh905@gmail.com>
2020-11-02 14:52:34 +0900

    search bt

--- Bluetooth_example/app/src/main/java/com/example/bluetooth_example/MainActivity.kt
+++ Bluetooth_example/app/src/main/java/com/example/bluetooth_example/MainActivity.kt
@@ -2,58 +2,101 @@ package com.example.bluetooth_example
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.content.BroadcastReceiver
-import android.content.Context
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-    private val REQUEST_ENABLE_BT = 1000
-
-    private val receiver = object : BroadcastReceiver() {
-
-        override fun onReceive(context: Context, intent: Intent) {
-            val action: String? = intent.action
-            when(action) {
-                BluetoothDevice.ACTION_FOUND -> {
-                    // Discovery has found a device. Get the BluetoothDevice
-                    // object and its info from the Intent.
-                    val device: BluetoothDevice? =
-                            intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    val deviceName = device!!.name
-                    val deviceHardwareAddress = device.address // MAC address
-                    Log.d("bt_name", deviceName)
-                    Log.d("bt_tag", deviceHardwareAddress)
-                }
-            }
-        }
-    }
-
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("#main", "on create")
 
-        // 블루투스 지원
         if (bluetoothAdapter == null) {
             // Device doesn't support Bluetooth
         }
 
-        // 블루투스 연결 창
         if (bluetoothAdapter?.isEnabled == false) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            startActivityForResult(enableBtIntent, 1000)
         }
 
+//        when (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+//            PackageManager.PERMISSION_GRANTED -> bluetoothLeScanner.startScan(bleScanner)
+//            else -> requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+//        }
+
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        pairedDevices?.forEach { device ->
+            val deviceName = device.name
+            val deviceHardwareAddress = device.address // MAC address
+            Log.e("#main_conneted", deviceName + deviceHardwareAddress)
+        }
 
 //        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
 //        registerReceiver(receiver, filter)
+//
+//        val ret = bluetoothAdapter!!.startDiscovery()
+//        Log.e("#main", ret.toString())
+
+        scanLeDevice(true)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(receiver)
+//        unregisterReceiver(receiver)
+    }
+
+    private var mScanning: Boolean = false
+    private var arrDevices = ArrayList<BluetoothDevice>()
+    private val handler = Handler(Looper.getMainLooper())
+    private val scanCallback = object: ScanCallback(){
+        override fun onScanFailed(errorCode: Int) {
+            super.onScanFailed(errorCode)
+            Log.e("#main", "scan failed " + errorCode)
+        }
+
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            result?.let{
+                if(!arrDevices.contains(it.device)) {
+                    arrDevices.add(it.device)
+                    Log.d("#main", it.device.toString() + " " + it.device.name)
+                    Log.d("#main", it.scanRecord!!.deviceName.toString())
+                }
+            }
+        }
+
+        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+            results?.let{
+                for (result in it){
+                    if(!arrDevices.contains(result.device)) arrDevices.add(result.device)
+                }
+            }
+        }
+    }
+    private val SCAN_PERIOD: Long = 10000
+    private fun scanLeDevice(enable: Boolean){
+        when (enable){
+            true -> {
+                handler.postDelayed({
+                    mScanning = false
+                    bluetoothAdapter!!.bluetoothLeScanner.stopScan(scanCallback)
+                }, SCAN_PERIOD)
+                mScanning = true
+                arrDevices.clear()
+                bluetoothAdapter!!.bluetoothLeScanner.startScan(scanCallback)
+            }
+            else ->{
+                mScanning = false
+                bluetoothAdapter!!.bluetoothLeScanner.startScan(scanCallback)
+            }
+        }
     }
 }
