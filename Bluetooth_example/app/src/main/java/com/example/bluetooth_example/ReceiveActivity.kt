9058ef591b26d8017d92816c498ff7c3e185832b
cho_yh <cyh905@gmail.com>
2020-11-03 13:36:17 +0900

    수신 데이터 view init

---
+++ Bluetooth_example/app/src/main/java/com/example/bluetooth_example/ReceiveActivity.kt
@@ -0,0 +1,10 @@
+package com.example.bluetooth_example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReceiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
