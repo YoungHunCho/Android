1a3574970c0bfed4dfe807991f42c1d5db80e127
cho_yh <cyh905@gmail.com>
2020-11-12 17:04:59 +0900

    애니메이션 첫번째 버전

--- Rotate_animation/app/src/main/java/com/example/rotate_animation/MainActivity.kt
+++ Rotate_animation/app/src/main/java/com/example/rotate_animation/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.rotate_animation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
+//https://itpangpang.xyz/271
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val iv = findViewById<ImageView>(R.id.iv)
+        val animation: Animation = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        iv.animation = animation
     }
 }
