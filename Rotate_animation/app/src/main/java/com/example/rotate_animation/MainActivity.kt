07aca934f46dd1a3e4ddc0ba6c907ab038033f0a
choraeng <dev.choyh@gmail.com>
2020-11-12 17:35:13 +0900

    두번째 방법

--- Rotate_animation/app/src/main/java/com/example/rotate_animation/MainActivity.kt
+++ Rotate_animation/app/src/main/java/com/example/rotate_animation/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.rotate_animation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.view.animation.RotateAnimation
 import android.widget.ImageView
 
 //https://itpangpang.xyz/271
@@ -14,6 +16,18 @@ class MainActivity : AppCompatActivity() {
 
         val iv = findViewById<ImageView>(R.id.iv)
         val animation: Animation = AnimationUtils.loadAnimation(this, R.anim.rotate)
-        iv.animation = animation
+//        iv.animation = animation
+        animate(iv, 0.toFloat(), 230.toFloat())
+        animate(iv, 230.toFloat(), 100.toFloat())
     }
+
+    private fun animate(view: View, fromDegree: Float, toDegree: Float) {
+        val animation: RotateAnimation =     RotateAnimation(fromDegree, toDegree,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f)
+        animation.duration = 200
+        animation.fillAfter = true
+        view.startAnimation(animation);
+    }
+
 }
